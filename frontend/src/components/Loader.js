ff26769b9a7eb2bf6b16e547ab5c6ed6ddc3e56f
Prayas Banerjee <pijushb1965@gmail.com>
2021-05-23 16:23:08 +0530

    Red Volunteers Page Included

--- frontend/src/components/Loader.js
+++ frontend/src/components/Loader.js
@@ -1,13 +1,37 @@
-import React from 'react';
-import { Spinner } from 'react-bootstrap';
+import React, { useState, useEffect } from 'react'
+import HashLoader from 'react-spinners/HashLoader'
+import { css } from "@emotion/react";
+
+
+const override = css`
+  display: block;
+  border-color: red;
+  text-allign: center;
+  position: fixed;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
 
 const Loader = () => {
-    return (
-        <Spinner animation='border' role='status' style={{ width: '100px', height: '100px', margin: 'auto', display: 'block' }}>
-            
-            <span className='sr-only'>Loading...</span>
 
-        </Spinner>
+    const [isLoading, setLoading] = useState(false);
+
+
+    useEffect(() => {
+        setLoading(true);
+        setTimeout(() => {
+        setLoading(false);
+        }, 3000)
+    }, []);
+
+    return (
+        <HashLoader color={"#123abc"} loading={isLoading} css={override}  size={150} />
+        // <Spinner className='loader-container' animation='border' role='status' style={{ width: '100px', height: '100px', margin: 'auto', display: 'block' }}>
+            
+        //     <span className='sr-only'>Loading...</span>
+
+        // </Spinner>
     )
 }
 
---
+++ frontend/src/screens/RedVolunteersScreen.js
@@ -0,0 +1,150 @@
+import React from 'react'
+import { LinkContainer } from 'react-router-bootstrap';
+
+const RedVolunteersScreen = () => {
+    return (
+        <>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    <LinkContainer to='/redVolunteersWB/districts/Alipurduar'>
+                        <h1>Alipurduar</h1>
+                    </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                <LinkContainer to='/redVolunteersWB/districts/Bankura'>
+                    <h1>Bankura</h1>
+                </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                    <LinkContainer to='/redVolunteersWB/districts/Birbhum'>
+                        <h1>Birbhum</h1>
+                    </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                    <LinkContainer to='/redVolunteersWB/districts/Cooch-Bihar'>
+                    <h1>Cooch Bihar</h1>
+                    </LinkContainer>
+                </div>
+            </div>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    <LinkContainer to='/redVolunteersWB/districts/Cooch-Bihar'>
+                    <h1>Cooch Bihar</h1>
+                    </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                    <LinkContainer to='/redVolunteersWB/districts/Dakshin-Dinajpur'>
+                        <h1>Dakshin Dinajpur</h1>
+                    </LinkContainer>
+                    
+                </div>
+                <div className="card card-red-details">
+                    <LinkContainer to='/redVolunteersWB/districts/Darjeeling'>
+                    <h1>Darjeeling</h1>
+                    </LinkContainer>
+                    
+                </div>
+            </div>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    <LinkContainer to='/redVolunteersWB/districts/Hooghly'>
+                        <h1>Hooghly</h1>
+                    </LinkContainer>
+                    
+                </div>
+                <div className="card card-red-details">
+                    
+                    <LinkContainer to='/redVolunteersWB/districts/Howrah'>
+                        <h1>Howrah</h1>
+                    </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                    
+                    <LinkContainer to='/redVolunteersWB/districts/Jalpaiguri'>
+                    <h1>Jalpaiguri</h1>
+                    </LinkContainer>
+                </div>
+            </div>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    
+                    <LinkContainer to='/redVolunteersWB/districts/Jhargram'>
+                    <h1>Jhargram</h1>
+                    </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                    
+                    <LinkContainer to='/redVolunteersWB/districts/Kalimpong'>
+                    <h1>Kalimpong</h1>
+                    </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                    
+                    <LinkContainer to='/redVolunteersWB/districts/Kolkata'>
+                    <h1>Kolkata</h1>
+                    </LinkContainer>
+                </div>
+            </div>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    
+                    <LinkContainer to='/redVolunteersWB/districts/Jhargram'>
+                    <h1>Jhargram</h1>
+                    </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                    
+                    <LinkContainer to='/redVolunteersWB/districts/Kalimpong'>
+                    <h1>Kalimpong</h1>
+                    </LinkContainer>
+                </div>
+                <div className="card card-red-details">
+                    <h1>Kolkata</h1>
+                </div>
+            </div>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    <h1>Malda</h1>
+                </div>
+                <div className="card card-red-details">
+                    <h1>Murshidabad</h1>
+                </div>
+                <div className="card card-red-details">
+                    <h1>Nadia</h1>
+                </div>
+            </div>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    <h1>North 24 Parganas</h1>
+                </div>
+                <div className="card card-red-details">
+                    <h1>Paschim Bardhaman</h1>
+                </div>
+                <div className="card card-red-details">
+                    <h1>Paschim Medinipur</h1>
+                </div>
+            </div>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    <h1>Purba Bardhaman</h1>
+                </div>
+                <div className="card card-red-details">
+                    <h1>Purba Medinipur</h1>
+                </div>
+                <div className="card card-red-details">
+                    <h1>Purulia</h1>
+                </div>
+            </div>
+            <div className='card-red'>
+                <div className="card card-red-details">
+                    <h1>South 24 Parganas</h1>
+                </div>
+                <div className="card card-red-details">
+                    <h1>Uttar Dinajpur</h1>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default RedVolunteersScreen
