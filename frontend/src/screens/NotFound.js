731250e2f8f9182e2a045f0d7dbe656ca6d56e6d
Prayas Banerjee <rupanreal7@gmail.com>
2021-05-29 14:21:40 +0530

    Readme File Introduced

--- frontend/src/screens/NotFound.js
+++ frontend/src/screens/NotFound.js
@@ -1,13 +1,20 @@
 import React from 'react'
+import {
+    useLocation
+  } from "react-router-dom";
 import { LinkContainer } from 'react-router-bootstrap'
 
 const NotFound = () => {
+    let location = useLocation();
     return (
         <>
             <div className='back-home'>
                 <LinkContainer to='/'>
                     <button className='btn'>Bact to Home</button>
                 </LinkContainer>
+                <h3>
+                    No match for <code>{location.pathname}</code>
+                </h3>
             </div>
             <div className='not-found'>
                 
