95c6b391dd3f9fc52be52a13dc3f9923ede4805d
Prayas Banerjee <rupanreal7@gmail.com>
2021-06-09 00:29:21 +0530

    Some UI Changes

---
+++ frontend/src/screens/AmbulanceScreen.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Construction from '../components/UnderConstruction.js'
+
+
+const AmbulanceScreen = () => {
+    return (
+        <div>
+            <Construction />
+        </div>
+    )
+}
+
+export default AmbulanceScreen
